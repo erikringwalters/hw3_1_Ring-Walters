43ad5985745f6fb5d41d06b5d77097507d464260
erikrw <ering-walters@csumb.edu>
2016-09-12 12:20:12 -0700

    First Commit

---
+++ hw3_1_Ring-Walters/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//	for (int i = 0;i < 100;i++)
+//	{
+//		cout << "its ya boy" << endl;
+//
+//
+//		cout << "           ___" << endl;
+//cout << "          /`   `'." << endl;
+//cout << "         /   _..---;" << endl;
+//cout << "         |  /__..._/  .--.-." << endl;
+//cout << "         |.'  e e | ___\_|/____" << endl;
+//cout << "        (_)'--.o.--|    | |    |" << endl;
+//cout << "       .-( `-' = `-|____| |____|" << endl;
+//cout << "      /  (         |____   ____|" << endl;
+//cout << "      |   (        |_   | |  __|" << endl;
+//cout << "      |    '-.--';/'/__ | | (  `|" << endl;
+//cout << "      |      '.   \    )"";--`\ /" << endl;
+//cout << "      \        ;   |--'    `;.-'" << endl;
+//cout << " jgs  |`-.__ ..-'--'`;..--'`" << endl;
+//	}
+
+	return 0;
+}
